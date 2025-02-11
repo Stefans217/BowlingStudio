17002a06b0e42d2ed28c8856d21a5c3be57b6e83
Stef <stefanspataro@hotmail.com>
2025-02-11 12:53:45 -0800

    implemented launch indicator

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -5,6 +5,8 @@ public class BallController : MonoBehaviour
 {
     [SerializeField] private float force = 1f;
     [SerializeField] private InputManager inputManager;
+    [SerializeField] private Transform ballAnchor;
+    [SerializeField] private Transform launchIndicator;
 
     public Rigidbody sphereRigidbody;
     private Rigidbody ballRB;
@@ -13,19 +15,20 @@ public class BallController : MonoBehaviour
 
     void Start()
     {
-        // Grabbing a reference to RigidBody
         ballRB = GetComponent<Rigidbody>();
-
-        // Add a listener to the OnSpacePressed event.
-        // When the space key is pressed, the
-        // LaunchBall method will be called.
         inputManager.OnSpacePressed.AddListener(LaunchBall);
+        transform.parent = ballAnchor;
+        transform.localPosition = Vector3.zero;
+        ballRB.isKinematic = true;
     }
 
     private void LaunchBall()
     {
         if (isBallLaunched) return;
         isBallLaunched = true;
-        ballRB.AddForce(transform.forward * force, ForceMode.Impulse);
+        transform.parent = null;
+        ballRB.isKinematic = false;
+        ballRB.AddForce(launchIndicator.forward * force, ForceMode.Impulse);
+        launchIndicator.gameObject.SetActive(false);
     }
 }
